cc5b66905dc8b42c20a3df0a7b9db97fc816ebbd
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-03 18:42:46 +0200

    added default string

--- snack2/index.php
+++ snack2/index.php
@@ -4,6 +4,10 @@ Passare come parametri GET name, mail e age e verificare
 di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 <?php
+$name = "Inserisci nome nei parametri";
+$mail = "Inserisci mail nei parametri";
+$age = "Inserisci età nei parametri";
+
 if (isset($_GET["name"])) {
     $name = $_GET["name"];
 
